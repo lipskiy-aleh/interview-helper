82af7bdeeaf7c816763d987aeef5a31a86e69b3c
aleh_lipski <olipskiy777@gmail.com>
2019-05-29 14:50:04 +0300

    Add redux store to app

--- apps/ui/src/components/App/App.js
+++ apps/ui/src/components/App/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
 import './App.css';
+
+import store from '../../redux'
+import history from '../../utils/history'
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div className="App">
+          Hello world
+        </div>
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
---
+++ apps/ui/src/redux/index.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, createStore } from 'redux'
+import { routerMiddleware } from 'connected-react-router'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createReducers from './reducers'
+import history from '../utils/history'
+
+const initialState = {}
+
+export default createStore(
+  createReducers(history),
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(
+      routerMiddleware(history),
+      thunk
+    )
+  )
+)
