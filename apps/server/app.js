be6bd237fe0da607fc863814af38ef4ea82a56da
aleh_lipski <olipskiy777@gmail.com>
2019-05-30 11:45:12 +0300

    Add logger to server app

--- apps/server/app.js
+++ apps/server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./src/routes');
 
 const app = express();
 const port = 3030;
 
+app.use(morgan('dev'));
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
