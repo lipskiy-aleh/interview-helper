672a83d85d730caf0d04e615aff84becd7f8aa50
aleh_lipski <olipskiy777@gmail.com>
2019-06-13 16:04:45 +0300

    Add error handler

--- apps/server/app.js
+++ apps/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const routes = require('./src/routes')
 
 const app = express()
@@ -8,8 +9,15 @@ const port = 3030
 
 app.use(morgan('dev'))
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/api', routes)
+
+app.use((err, req, res, next) => {
+  res
+    .status(err.status)
+    .send({ message: err.message })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
